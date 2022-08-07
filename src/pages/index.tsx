7e04c43a78ecedb95257869593ebd3d67b954fef
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2022-08-07 12:32:41 -0300

    Adds home cards component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import { Flex } from '@chakra-ui/react';
 
 import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
+import { HomeCards } from '../components/HomeCards';
 
 export default function Home() {
   return (
     <Flex direction='column'>
       <Header />
       <Banner />
+      <HomeCards />
     </Flex>
   );
 }
